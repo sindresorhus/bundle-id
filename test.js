bc23cc77af48fdb23eef17484cac1b0a8ba4e64f
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-30 13:44:45 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
 import m from '.';
 
-test('returns expected id', async t => {
+test('returns expected id - async', async t => {
 	t.is(await m('Safari'), 'com.apple.Safari');
 });
 
-test('sync returns expected id', t => {
+test('returns expected id - sync', t => {
 	t.is(m.sync('Safari'), 'com.apple.Safari');
 });
